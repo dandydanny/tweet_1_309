c25f63408fd6e936fed5970d6f845bbe75af0f70
Apprentice <apprentice@dbc-chicago034.local>
2013-11-11 19:50:50 -0600

    Not quite working yet...

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,10 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/:username' do
+  p params[:username]
+  @timeline = Twitter.user_timeline(params[:username])
+  @tweetarray = @timeline.map {|t| t.text}
+  erb :display10
+end
